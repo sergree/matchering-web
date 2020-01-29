59b127dff814a5a412f2b02c482996725feb7eac
Sergree <43672811+sergree@users.noreply.github.com>
2020-01-30 02:02:54 +0300

    Dockerfile and Supervisor config

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3-slim
+
+RUN apt update && apt -y install libsndfile1 ffmpeg redis-server supervisor
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+COPY . .
+
+CMD ["./init.sh"]
--- init.sh
+++ init.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-# Run this script using "source init.sh"
 
-if [ -z ${SECRET_KEY+x} ]; then
-    RND_VALUE=`date +%s|sha256sum|base64|head -c 50`
-    export SECRET_KEY="$RND_VALUE"
-    echo "export SECRET_KEY="$RND_VALUE"" >> ~/.bashrc
-fi
+export SECRET_KEY=`date +%s|sha256sum|base64|head -c 50`
 
+python3 manage.py flush --no-input
 python3 manage.py makemigrations mgw_back
 python3 manage.py migrate
 
 # https://docs.docker.com/config/containers/multi-service_container/
-set -m
-python3 manage.py runserver 0:8360 &
-python3 manage.py rqworker default 
-fg %1
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
