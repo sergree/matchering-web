c72396f44cb9641ae2c713a838e89c8f8578c291
Sergree <43672811+sergree@users.noreply.github.com>
2020-01-30 16:59:12 +0300

    Linux build support

--- Dockerfile
+++ Dockerfile
@@ -24,5 +24,6 @@ COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+RUN chmod +x ./init.sh && mkdir db
 
 CMD ["./init.sh"]
--- init.sh
+++ init.sh
@@ -18,7 +18,6 @@
 
 export SECRET_KEY=`date +%s|sha256sum|base64|head -c 50`
 
-python3 manage.py flush --no-input
 python3 manage.py makemigrations mgw_back
 python3 manage.py migrate
 
