a4af7e2ba3571347e87405d1cfa076e29e2c1b0a
Sergree <43672811+sergree@users.noreply.github.com>
2020-01-30 12:15:32 +0300

    GPLv3

--- init.sh
+++ init.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Matchering WEB - Handy Matchering 2.0 Containerized Web Application
+# Copyright (C) 2016-2020 Sergree
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 export SECRET_KEY=`date +%s|sha256sum|base64|head -c 50`
 
 python3 manage.py flush --no-input
