1f8284b5c0fed1f0e53ac80c591853d785964b62
Sergey Grishakov <infextro@gmail.com>
2022-04-13 17:08:58 +0300

    2.0.4 update

--- mgw_back/urls.py
+++ mgw_back/urls.py
@@ -1,6 +1,6 @@
 """
 Matchering WEB - Handy Matchering 2.0 Containerized Web Application
-Copyright (C) 2016-2021 Sergree
+Copyright (C) 2016-2022 Sergree
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
